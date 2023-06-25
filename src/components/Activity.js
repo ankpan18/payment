66b42bf269212ce79238c7c3f9f379ea981cda0e
Ankur Panthri <panthriankur@outlook.com>
2023-06-25 23:51:23 +0530

    Version 1.0

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,24 +1,179 @@
-import React from 'react';
-import {ReactComponent as Search} from '../assets/magnifying-glass-solid.svg';
-import {ReactComponent as Down} from '../assets/download-solid.svg';
-import '../styles/Activity.css';
+import {React,useEffect,useState} from "react";
+import { ReactComponent as Search } from "../assets/magnifying-glass-solid.svg";
+import { ReactComponent as Down } from "../assets/download-solid.svg";
+import "../styles/Activity.css";
+import items from '../assets/MOCK_DATA.json';
 
 function Activity() {
-  return (
-    <div className='activity_box'>
-        <div className='first'>
-            <div className='input_box'>
-                <Search id='search_btn'/>
-                
-                <input type='text' placeholder='Search by name or email'/>
-            </div>
-            <div id='download_btn'>
-                <Down />
-            </div>
 
+    // const []=
+    const [selectedItems,setSelectedItems]=useState(items);
+
+    var filter_sadness={"Date":{options:["Last 30 days","Last 90 days"],selected:-1},
+    "Type":{options:["Sent","Received"],selected:-1},
+    "Status":{options:["failed","completed","pending"],selected:-1}
+    }
+    
+    const [Filters,setFilters]=useState(filter_sadness);
+    const [test,settest]=useState("");
+                
+    // console.log(Object.keys(Filters));
+
+    useEffect(()=>{
+        // console.log(Filters);
+        filterItems();
+        
+        
+    },[test]);
+
+    function filterItems(){
+        
+        var cat="";
+        var final_items=Object.keys(Filters).map((filter)=>{
+            var temp_items=[];
+            // {console.log(filter);}
+            if(Filters[filter].selected!==-1){
+                // {console.log(filter);}
+                if(filter==="Type"){
+                    var cat=Filters[filter].options[Filters[filter].selected];  // cat => category
+                    if (cat==="Sent"){
+                        temp_items=items.filter((item)=>{
+                        return (item.type==="sent");
+                        })
+                    }
+                    else if(cat==="Received"){
+                        temp_items=items.filter((item)=>{
+                        return (item.type==="received");
+                        })
+                    }
+                    // console.log(temp);
+                }
+                else if(filter==="Status"){
+                    var cat=Filters[filter].options[Filters[filter].selected];  // cat => category
+                    if (cat==="pending"){
+                        temp_items=items.filter((item)=>{
+                            return (item.status==="pending");
+                        })
+                    }
+                    else if(cat==="completed"){
+                        temp_items=items.filter((item)=>{
+                            return (item.status==="completed");
+                        })
+                    }
+                    else if(cat==="failed"){
+                        temp_items=items.filter((item)=>{
+                            return (item.status==="failed");
+                        })
+                    }
+                }
+                // setSelectedItems()
+                return temp_items;
+            }
+            else{
+                return items;
+            }
+        })
+        
+        var final_final_items = final_items[0].filter(value => final_items[1].includes(value));
+        final_final_items = final_final_items.filter(value => final_items[2].includes(value));
+
+        // for final_items
+        console.log(final_final_items);
+        setSelectedItems(final_final_items);
+    }
+    function toggle_options(event){
+        event.target?.closest(".filter_btn").querySelector(".options").classList.toggle("hidden");
+        
+    }
+    function changeFilter(event){
+        // console.log("changeFilter");
+        var option=event.target?.innerHTML;
+        var filter=event.target?.closest(".filter_btn").querySelector("button").innerHTML;
+        // console.log(filter,":",option);
+        var filters=Filters;
+        filters[filter].selected=(filters[filter].options.indexOf(option));
+        setFilters(filters);
+        settest(JSON.stringify(filters));
+        // console.log(Filters);
+    }
+    function create_card(item){
+        return (
+        <div className="card" key={item["id"]}>
+          <div className="main">
+            <img
+              src={item['image']}
+              alt="company_profile"
+            />
+            <div className="details">
+              <b>{item['name']}</b>
+              <div className="sub_detail">
+                <p>{item['date']} ·</p>
+                <p>Automatic Payment</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="value">
+            <b>${item['payment']}</b>
+          </div>
         </div>
+        );
+    }
+  return (
+    <div className="activity_box">
+      <div className="first">
+        <div className="input_box">
+          <Search id="search_btn" />
+
+          <input type="text" placeholder="Search by name or email" />
+        </div>
+        <div id="download_btn">
+          <Down />
+        </div>
+      </div>
+
+      <div className="second">
+        <p>Filter by</p>
+
+        <div className="filter_btn_container">
+
+        {Object.keys(Filters).map((filter)=>{
+            var filter_details=Filters[filter];
+            return (
+                <div key={filter} className="filter_btn" onClick={toggle_options}>
+                    <button className="active">{filter}</button>
+                    <div className="options hidden">
+                        {
+                        filter_details.options.map((option,idx)=>{
+                            return(
+                                <button key={idx} onClick={changeFilter} className="active">{option}</button>
+                                
+                            )            
+                            })
+                        }
+                        {/* <hr/> */}
+                        
+                    </div>
+                </div>
+            );
+        })}
+           
+          
+        </div>
+      </div>
+
+      <div className="third">
+        <h2>Transaction</h2>
+        {
+            selectedItems.map((item,idx)=>{
+                return create_card(item);
+            })
+        }
+        
+
+      </div>
     </div>
-  )
+  );
 }
 
-export default Activity
+export default Activity;
